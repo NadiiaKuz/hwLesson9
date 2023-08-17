e323ea0328abaa3e519d5929b8fffd43c59a7d3a
Nadiia <nadink1603@gmail.com>
2023-08-17 15:02:39 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace hwLesson9
+{
+    static class Calculator
+    {
+        public static double Mul(double a, double b) =>
+            a * b;
+
+        public static double Div(double a, double b) =>
+           (double)a / b;
+
+        public static double Add(double a, double b) =>
+            a + b;
+
+        public static double Sub(double a, double b) =>
+            a - b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace hwLesson9
+{
+    public delegate double Calculate(double x, double y);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input A:");
+                double a = Double.Parse(Console.ReadLine());
+
+                Console.Write("Input B:");
+                double b = Double.Parse(Console.ReadLine());
+
+                Console.Write("Input the sign of the math operation: ");
+                char operation = Char.Parse(Console.ReadLine());
+
+                if (operation == '/' && b == 0)
+                {
+                    Console.WriteLine("Divided by 0");
+                    continue;
+                }
+
+                Calculate calculate = operation switch
+                {
+                    '+' => (a, b) => Calculator.Add(a, b),
+                    '-' => (a, b) => Calculator.Sub(a, b),
+                    '*' => (a, b) => Calculator.Mul(a, b),
+                    '/' => (a, b) => Calculator.Div(a, b),
+                };
+
+                Console.WriteLine($"{a} {operation} {b} = {calculate(a, b)}");
+            }
+        }
+    }
+}
